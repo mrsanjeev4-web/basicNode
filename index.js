2ae5c98615ed947da59fe776bfa55f3f8e644f12
mrsanjeev4-web <mrsanjeev4@gmail.com>
2025-08-24 21:27:45 +0530

    Fix - index file updated .

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
-// index.js
+
 const express = require("express");
 const mongoose = require("mongoose");
 
 const app = express();
 const PORT = 5000;
 
-// MongoDB connection
+
 mongoose.connect("mongodb://127.0.0.1:27017/testDB")
   .then(() => console.log("✅ MongoDB Connected via Mongoose!"))
   .catch(err => console.error("❌ DB Connection Error:", err));
 
-// Schema & Model
+
 const userSchema = new mongoose.Schema({
   name: String,
   email: String,
@@ -19,10 +19,10 @@ const userSchema = new mongoose.Schema({
 
 const User = mongoose.model("User", userSchema);
 
-// Middleware
+
 app.use(express.json());
 
-// Routes
+
 app.get("/", (req, res) => {
   res.send("Hello from Express + Mongoose!");
 });
@@ -38,7 +38,7 @@ app.get("/users", async (req, res) => {
   res.send(users);
 });
 
-// Start server
+
 app.listen(PORT, () => {
   console.log(`🚀 Server running on http://localhost:${PORT}`);
 });
